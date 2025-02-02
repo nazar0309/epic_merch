36fa8dc9301aec81d3c93386c7e50c90106fcdb4
Nazar Tsuper <nazartsuper0309@gmail.com>
2025-02-02 22:27:04 +0000

    Add style to profile form

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
-from django.shortcuts import render
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'on_profile_page': True
+    }
 
     return render(request, template, context)
