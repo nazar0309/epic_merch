9663210835458ed95bcb8e77fce16211502fc43f
Nazar Tsuper <nazartsuper0309@gmail.com>
2025-02-12 20:36:53 +0000

    Fix custom widget handling to show image

--- products/views.py
+++ products/views.py
@@ -107,12 +107,13 @@ def add_product(request):
 
 @login_required
 def edit_product(request, product_id):
-    """ Edit a product in the store """  
+    """ Edit a product in the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
     
     product = get_object_or_404(Product, pk=product_id)
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
