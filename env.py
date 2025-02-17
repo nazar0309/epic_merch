a153ac2f79d357dcb13189a62fce7ab07857e17a
Nazar Tsuper <nazartsuper0309@gmail.com>
2025-02-17 20:25:22 +0000

    Add env.py to the repository

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault('DEVELOPMENT', '1')
+os.environ.setdefault('SECRET_KEY', 'NHIU£43inbdI123213nUIO2322')
+os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51QkA8y09i9j5PbfJEc8EpZ8yaLEM9kXmvx9knQMQrxQIOmKpQkLnTJdx4nksfZAZo6g2eH1ySdI1wW2t2NUOtVVa002mSUPsIc')
+os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51QkA8y09i9j5PbfJxN9KqUcUDJimzlypSQQYSzPXWmZTKYL6qrb59ZiBDP2RMHBwDTflWnU838iNjs8lR5DKCpBN00JVaTZ4Y6')
+os.environ.setdefault('STRIPE_WH_SECRET', 'whsec_rLgJx5rRe2hvz46eZzDjtM1JVf49jFxU')
+os.environ.setdefault('DATABASE_URL', ' postgresql://neondb_owner:iRNmox1eaj7I@ep-nameless-waterfall-a2mq6567.eu-central-1.aws.neon.tech/agent_upon_mural_301297')
