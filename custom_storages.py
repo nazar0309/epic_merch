ef91316d4f886de571666459c715817c1a005a16
Nazar Tsuper <nazartsuper0309@gmail.com>
2025-02-18 23:17:39 +0000

    Change json file in s3

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
-
+    location = 'static'  # Specify the directory for static files
+    file_overwrite = False  # To prevent overwriting files
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = 'media'  # Specify the directory for media files
+    file_overwrite = False  # To prevent overwriting files
