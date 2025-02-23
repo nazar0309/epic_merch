72de856f02577ae7eafe1d2a80f9d22083517a9b
Nazar Tsuper <nazartsuper0309@gmail.com>
2025-02-23 19:57:16 +0000

    reset email host pass

--- env.py
+++ env.py
@@ -6,5 +6,5 @@ os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51QkA8y09i9j5PbfJIBoixbVcrsi
 os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51QkA8y09i9j5PbfJ3RyExmeq1poFHlU0LmFlusr899eblOE0ez3thJ1pI6b9CAwr7vCwWv0plqlnhh5fXaKzrpIV00WMybr3Xl')
 os.environ.setdefault('STRIPE_WH_SECRET', 'whsec_rLgJx5rRe2hvz46eZzDjtM1JVf49jFxU')
 os.environ.setdefault('DATABASE_URL', ' postgresql://neondb_owner:iRNmox1eaj7I@ep-nameless-waterfall-a2mq6567.eu-central-1.aws.neon.tech/agent_upon_mural_301297')
-os.environ.setdefault('EMAIL_HOST_PASS', 'cdvyjxlgyzvvfqcn')
+os.environ.setdefault('EMAIL_HOST_PASS', 'glwodvudcoouucv')
 os.environ.setdefault('EMAIL_HOST_USER', 'nazartsuper0309@gmail.com')
