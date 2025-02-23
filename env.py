4dec0f9c220a6cbfa4143284f6e8d18cb6833452
Nazar Tsuper <nazartsuper0309@gmail.com>
2025-02-23 20:15:23 +0000

    Set deployment to 0

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault('DEVELOPMENT', '1')
+os.environ.setdefault('DEVELOPMENT', '0')
 os.environ.setdefault('SECRET_KEY', 'NHIU£43inbdI123213nUIO2322')
 os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51QkA8y09i9j5PbfJIBoixbVcrsiqnY4XvYn3QJ71D0tA5Ykw7T9sS2iSaCvEYQU6FatxApBHhwWfKneFZ3yqaWiE00Edu5lcSt')
 os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51QkA8y09i9j5PbfJ3RyExmeq1poFHlU0LmFlusr899eblOE0ez3thJ1pI6b9CAwr7vCwWv0plqlnhh5fXaKzrpIV00WMybr3Xl')
