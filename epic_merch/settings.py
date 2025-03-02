9c31ee9716b0b5712ca701c810f581004be3977e
Nazar Tsuper <nazartsuper0309@gmail.com>
2025-03-02 18:00:01 +0000

    Set debug to False

--- epic_merch/settings.py
+++ epic_merch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'epic-merch-565735eb773a.herokuapp.com',
